42289c48f6960202b27070d95420c291485f81fd
Kārlis <kmendzins@gmail.com>
2016-10-12 13:50:04 +0300

    style.css --> different .scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('*.scss')
+    gulp.src('css/*.scss')
         .pipe(sass())
         .pipe(gulp.dest(function(f) {
             return f.base;
@@ -10,5 +10,5 @@ gulp.task('sass', function() {
 });
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('css/*.scss', ['sass']);
+    gulp.watch('**/*.scss', ['sass']);
 })
