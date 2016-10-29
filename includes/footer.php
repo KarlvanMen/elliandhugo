ae95f8a97009f86a244e60cfe018cd2d054c35f4
Karlis Mendz <kmendzins@gmail.com>
2016-10-29 20:20:07 +0300

    Mobile layout done

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,11 @@
 		<li><a href="/elliandhugo/?page=misija">Vīzija un misija</a></li>
 	</ul>
 	<h4>Atrodi mūs</h4>
-	
+	<div class="social">
+		<a href="https://www.facebook.com/elliandhugo/"><img src="img/F_icon.svg" alt="Facebook"></a>	
+		<a href="https://www.instagram.com/elliandhugo/"><img src="img/I_icon.svg" alt="Instagram"></a>	
+		<a href="https://www.pinterest.com/dacemortukane/elli-hugo/"><img src="img/P_icon.svg" alt="Pinterest"></a>		
+	</div>
 	<h4>Kontakti</h4>
 	<div class="footer-bottom"><p>Elli & Hugo</p> <a href="mailto:kontakti@elliandhugo.lv">kontakti@elliandhugo.lv</a></div>
 	<div class="copyright">Copyright ©2016 Elli&Hugo</div>
